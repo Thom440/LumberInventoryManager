23b4190983f9d1055b8a6f03595c18e7fdce2578
Thom Barrett <thom440@yahoo.com>
2021-01-28 13:47:03 -0800

    Created Add Window Form

---
+++ LumberInventoryManager/AddProduct.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace LumberInventoryManager
+{
+    partial class AddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.heightTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.widthTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lengthTxtBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Height";
+            // 
+            // heightTxtBox
+            // 
+            this.heightTxtBox.Location = new System.Drawing.Point(98, 5);
+            this.heightTxtBox.MaxLength = 2;
+            this.heightTxtBox.Name = "heightTxtBox";
+            this.heightTxtBox.Size = new System.Drawing.Size(47, 20);
+            this.heightTxtBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Product Width";
+            // 
+            // widthTxtBox
+            // 
+            this.widthTxtBox.Location = new System.Drawing.Point(98, 34);
+            this.widthTxtBox.MaxLength = 2;
+            this.widthTxtBox.Name = "widthTxtBox";
+            this.widthTxtBox.Size = new System.Drawing.Size(47, 20);
+            this.widthTxtBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Product Length";
+            // 
+            // lengthTxtBox
+            // 
+            this.lengthTxtBox.Location = new System.Drawing.Point(98, 65);
+            this.lengthTxtBox.MaxLength = 2;
+            this.lengthTxtBox.Name = "lengthTxtBox";
+            this.lengthTxtBox.Size = new System.Drawing.Size(47, 20);
+            this.lengthTxtBox.TabIndex = 5;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(16, 109);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 6;
+            this.addBtn.Text = "Add Product";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(107, 109);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 7;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 149);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.lengthTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.widthTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.heightTxtBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddProduct";
+            this.Text = "Add Product";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox heightTxtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox widthTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lengthTxtBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
