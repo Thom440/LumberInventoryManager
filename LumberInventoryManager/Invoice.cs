8a8af787cab7fedfae09f67b69f282ffe5133f68
Kramer Jensen <Kramerjensend@gmail.com>
2021-02-04 14:02:56 -0800

    Created Invoice Table

---
+++ LumberInventoryManager/Invoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumberInventoryManager
+{
+    public class Invoice
+    {
+        [Key]
+        public int InvoiceID { get; set; }
+
+        public int InvoiceNumber { get; set; }
+
+        [Column(TypeName = "smalldatetime")]
+        public DateTime InvoiceDate { get; set; }
+
+        [Column(TypeName = "smalldatetime")]
+        public DateTime ShipDate { get; set; }
+    }
+}
