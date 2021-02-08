37adca2c5f59c06a02e75e844322ef40ea00c83c
Thom Barrett <thom440@yahoo.com>
2021-02-08 14:34:24 -0800

    Add seed data to populate the Category table

--- LumberInventoryManager/Migrations/Configuration.cs
+++ LumberInventoryManager/Migrations/Configuration.cs
@@ -18,6 +18,14 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            context.Categories.AddOrUpdate(x => x.CategoryID,
+                new Category() { CategoryID = 1, CategoryName = "WW" },
+                new Category() { CategoryID = 2, CategoryName = "ACQ" },
+                new Category() { CategoryID = 3, CategoryName = "CCA" },
+                new Category() { CategoryID = 4, CategoryName = ".25" },
+                new Category() { CategoryID = 5, CategoryName = ".40" },
+                new Category() { CategoryID = 6, CategoryName = ".60" }
+                );
         }
     }
 }
