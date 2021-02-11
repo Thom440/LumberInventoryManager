bd05aed2bdb80cb1c8223d9e99ac59f6d7b38f60
Thom Barrett <thom440@yahoo.com>
2021-02-10 20:51:16 -0800

    Added categories to the Add Form

--- LumberInventoryManager/Form/AddProduct.Designer.cs
+++ LumberInventoryManager/Form/AddProduct.Designer.cs
@@ -38,6 +38,12 @@ namespace LumberInventoryManager
             this.addBtn = new System.Windows.Forms.Button();
             this.cancelBtn = new System.Windows.Forms.Button();
             this.messageLbl = new System.Windows.Forms.Label();
+            this.wwCheckBox = new System.Windows.Forms.CheckBox();
+            this.twoFiveCheckBox = new System.Windows.Forms.CheckBox();
+            this.fourOCheckBox = new System.Windows.Forms.CheckBox();
+            this.sixOCheckBox = new System.Windows.Forms.CheckBox();
+            this.acqCheckBox = new System.Windows.Forms.CheckBox();
+            this.ccaCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -93,7 +99,7 @@ namespace LumberInventoryManager
             // 
             // addBtn
             // 
-            this.addBtn.Location = new System.Drawing.Point(16, 109);
+            this.addBtn.Location = new System.Drawing.Point(12, 166);
             this.addBtn.Name = "addBtn";
             this.addBtn.Size = new System.Drawing.Size(75, 23);
             this.addBtn.TabIndex = 6;
@@ -103,7 +109,7 @@ namespace LumberInventoryManager
             // 
             // cancelBtn
             // 
-            this.cancelBtn.Location = new System.Drawing.Point(107, 109);
+            this.cancelBtn.Location = new System.Drawing.Point(98, 166);
             this.cancelBtn.Name = "cancelBtn";
             this.cancelBtn.Size = new System.Drawing.Size(75, 23);
             this.cancelBtn.TabIndex = 7;
@@ -114,17 +120,89 @@ namespace LumberInventoryManager
             // messageLbl
             // 
             this.messageLbl.AutoSize = true;
-            this.messageLbl.Location = new System.Drawing.Point(1, 139);
+            this.messageLbl.Location = new System.Drawing.Point(9, 198);
             this.messageLbl.Name = "messageLbl";
             this.messageLbl.Size = new System.Drawing.Size(0, 13);
             this.messageLbl.TabIndex = 8;
+            // 
+            // wwCheckBox
+            // 
+            this.wwCheckBox.AutoSize = true;
+            this.wwCheckBox.Location = new System.Drawing.Point(12, 97);
+            this.wwCheckBox.Name = "wwCheckBox";
+            this.wwCheckBox.Size = new System.Drawing.Size(86, 17);
+            this.wwCheckBox.TabIndex = 9;
+            this.wwCheckBox.Text = "White Wood";
+            this.wwCheckBox.UseVisualStyleBackColor = true;
+            this.wwCheckBox.CheckedChanged += new System.EventHandler(this.wwCheckBox_CheckedChanged);
+            // 
+            // twoFiveCheckBox
+            // 
+            this.twoFiveCheckBox.AutoSize = true;
+            this.twoFiveCheckBox.Location = new System.Drawing.Point(12, 120);
+            this.twoFiveCheckBox.Name = "twoFiveCheckBox";
+            this.twoFiveCheckBox.Size = new System.Drawing.Size(41, 17);
+            this.twoFiveCheckBox.TabIndex = 10;
+            this.twoFiveCheckBox.Text = ".25";
+            this.twoFiveCheckBox.UseVisualStyleBackColor = true;
+            this.twoFiveCheckBox.CheckedChanged += new System.EventHandler(this.twoFiveCheckBox_CheckedChanged);
+            // 
+            // fourOCheckBox
+            // 
+            this.fourOCheckBox.AutoSize = true;
+            this.fourOCheckBox.Location = new System.Drawing.Point(57, 120);
+            this.fourOCheckBox.Name = "fourOCheckBox";
+            this.fourOCheckBox.Size = new System.Drawing.Size(41, 17);
+            this.fourOCheckBox.TabIndex = 11;
+            this.fourOCheckBox.Text = ".40";
+            this.fourOCheckBox.UseVisualStyleBackColor = true;
+            this.fourOCheckBox.CheckedChanged += new System.EventHandler(this.fourOCheckBox_CheckedChanged);
+            // 
+            // sixOCheckBox
+            // 
+            this.sixOCheckBox.AutoSize = true;
+            this.sixOCheckBox.Location = new System.Drawing.Point(98, 120);
+            this.sixOCheckBox.Name = "sixOCheckBox";
+            this.sixOCheckBox.Size = new System.Drawing.Size(41, 17);
+            this.sixOCheckBox.TabIndex = 12;
+            this.sixOCheckBox.Text = ".60";
+            this.sixOCheckBox.UseVisualStyleBackColor = true;
+            this.sixOCheckBox.CheckedChanged += new System.EventHandler(this.sixOCheckBox_CheckedChanged);
+            // 
+            // acqCheckBox
+            // 
+            this.acqCheckBox.AutoSize = true;
+            this.acqCheckBox.Location = new System.Drawing.Point(12, 143);
+            this.acqCheckBox.Name = "acqCheckBox";
+            this.acqCheckBox.Size = new System.Drawing.Size(48, 17);
+            this.acqCheckBox.TabIndex = 13;
+            this.acqCheckBox.Text = "ACQ";
+            this.acqCheckBox.UseVisualStyleBackColor = true;
+            this.acqCheckBox.CheckedChanged += new System.EventHandler(this.acqCheckBox_CheckedChanged);
+            // 
+            // ccaCheckBox
+            // 
+            this.ccaCheckBox.AutoSize = true;
+            this.ccaCheckBox.Location = new System.Drawing.Point(66, 143);
+            this.ccaCheckBox.Name = "ccaCheckBox";
+            this.ccaCheckBox.Size = new System.Drawing.Size(47, 17);
+            this.ccaCheckBox.TabIndex = 14;
+            this.ccaCheckBox.Text = "CCA";
+            this.ccaCheckBox.UseVisualStyleBackColor = true;
+            this.ccaCheckBox.CheckedChanged += new System.EventHandler(this.ccaCheckBox_CheckedChanged);
             // 
             // AddProduct
             // 
             this.AcceptButton = this.addBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(204, 176);
+            this.ClientSize = new System.Drawing.Size(204, 221);
+            this.Controls.Add(this.ccaCheckBox);
+            this.Controls.Add(this.acqCheckBox);
+            this.Controls.Add(this.sixOCheckBox);
+            this.Controls.Add(this.fourOCheckBox);
+            this.Controls.Add(this.twoFiveCheckBox);
+            this.Controls.Add(this.wwCheckBox);
             this.Controls.Add(this.messageLbl);
             this.Controls.Add(this.cancelBtn);
             this.Controls.Add(this.addBtn);
@@ -152,5 +230,11 @@ namespace LumberInventoryManager
         private System.Windows.Forms.Button addBtn;
         private System.Windows.Forms.Button cancelBtn;
         private System.Windows.Forms.Label messageLbl;
+        private System.Windows.Forms.CheckBox wwCheckBox;
+        private System.Windows.Forms.CheckBox twoFiveCheckBox;
+        private System.Windows.Forms.CheckBox fourOCheckBox;
+        private System.Windows.Forms.CheckBox sixOCheckBox;
+        private System.Windows.Forms.CheckBox acqCheckBox;
+        private System.Windows.Forms.CheckBox ccaCheckBox;
     }
 }
