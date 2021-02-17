514782ffb713344a429e44b9c9f26af377f6cedc
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2021-02-16 20:24:39 -0800

    Created Add Customer Form (#62)

---
+++ LumberInventoryManager/Form/AddCustomerForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace LumberInventoryManager
+{
+    partial class AddCustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameTxtBox = new System.Windows.Forms.TextBox();
+            this.lastNameTxtBox = new System.Windows.Forms.TextBox();
+            this.addressTxtBox = new System.Windows.Forms.TextBox();
+            this.cityTxtBox = new System.Windows.Forms.TextBox();
+            this.stateTxtBox = new System.Windows.Forms.TextBox();
+            this.zipCodeTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.businessTxtBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNameTxtBox
+            // 
+            this.firstNameTxtBox.Location = new System.Drawing.Point(100, 43);
+            this.firstNameTxtBox.Name = "firstNameTxtBox";
+            this.firstNameTxtBox.Size = new System.Drawing.Size(122, 20);
+            this.firstNameTxtBox.TabIndex = 0;
+            // 
+            // lastNameTxtBox
+            // 
+            this.lastNameTxtBox.Location = new System.Drawing.Point(316, 43);
+            this.lastNameTxtBox.Name = "lastNameTxtBox";
+            this.lastNameTxtBox.Size = new System.Drawing.Size(119, 20);
+            this.lastNameTxtBox.TabIndex = 1;
+            // 
+            // addressTxtBox
+            // 
+            this.addressTxtBox.Location = new System.Drawing.Point(100, 97);
+            this.addressTxtBox.Name = "addressTxtBox";
+            this.addressTxtBox.Size = new System.Drawing.Size(220, 20);
+            this.addressTxtBox.TabIndex = 2;
+            // 
+            // cityTxtBox
+            // 
+            this.cityTxtBox.Location = new System.Drawing.Point(100, 153);
+            this.cityTxtBox.Name = "cityTxtBox";
+            this.cityTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.cityTxtBox.TabIndex = 3;
+            // 
+            // stateTxtBox
+            // 
+            this.stateTxtBox.Location = new System.Drawing.Point(280, 153);
+            this.stateTxtBox.MaxLength = 2;
+            this.stateTxtBox.Name = "stateTxtBox";
+            this.stateTxtBox.Size = new System.Drawing.Size(40, 20);
+            this.stateTxtBox.TabIndex = 4;
+            // 
+            // zipCodeTxtBox
+            // 
+            this.zipCodeTxtBox.Location = new System.Drawing.Point(100, 208);
+            this.zipCodeTxtBox.MaxLength = 5;
+            this.zipCodeTxtBox.Name = "zipCodeTxtBox";
+            this.zipCodeTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.zipCodeTxtBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Street Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "City:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(239, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "State:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Zip Code:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 28);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add Customer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 28);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // businessTxtBox
+            // 
+            this.businessTxtBox.Location = new System.Drawing.Point(100, 261);
+            this.businessTxtBox.Name = "businessTxtBox";
+            this.businessTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.businessTxtBox.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 267);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Business:";
+            // 
+            // AddCustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 304);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.businessTxtBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zipCodeTxtBox);
+            this.Controls.Add(this.stateTxtBox);
+            this.Controls.Add(this.cityTxtBox);
+            this.Controls.Add(this.addressTxtBox);
+            this.Controls.Add(this.lastNameTxtBox);
+            this.Controls.Add(this.firstNameTxtBox);
+            this.Name = "AddCustomerForm";
+            this.Text = "AddCustomerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameTxtBox;
+        private System.Windows.Forms.TextBox lastNameTxtBox;
+        private System.Windows.Forms.TextBox addressTxtBox;
+        private System.Windows.Forms.TextBox cityTxtBox;
+        private System.Windows.Forms.TextBox stateTxtBox;
+        private System.Windows.Forms.TextBox zipCodeTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox businessTxtBox;
+        private System.Windows.Forms.Label label7;
+    }
+}
