844f5141a352a07a57dfddde5375ca00e8d58513
Kramer Jensen <Kramerjensend@gmail.com>
2021-02-16 21:08:02 -0800

    Add Customer form is functional

---
+++ LumberInventoryManager/Migrations/202102170501593_ManyToManyInvoices.cs
@@ -0,0 +1,18 @@
+﻿namespace LumberInventoryManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ManyToManyInvoices : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "ZipCode", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "ZipCode", c => c.Short(nullable: false));
+        }
+    }
+}
